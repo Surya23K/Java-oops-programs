dd4bc2cf1f9e5a2ae3e2e946301258ad339779ec
Surya23K <suryaa232002@gmail.com>
2025-03-14 03:13:49 -0700

    Add files via upload

---
+++ Coursedetails.java
@@ -0,0 +1,8 @@
+public class Coursedetails {
+    public static void main(String[] args) {
+        int rollno=5;
+        String course="MCA";
+        System.out.println("Roll No:"+rollno);
+        System.out.println("Course:"+course);
+        }
+    }
