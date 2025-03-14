ed542988b8e60c02f55a2bde864b37f0f4046951
Surya23K <suryaa232002@gmail.com>
2025-03-14 03:16:41 -0700

    Add files via upload

---
+++ Emp1.java
@@ -0,0 +1,13 @@
+public class Emp1 
+{
+    void displayMsg()
+    {
+        System.out.println("Employee Details");
+    }
+    public static void main(String[] args) 
+        {
+            Emp1 ob=new Emp1();
+            ob.displayMsg();
+        }
+    }
+
