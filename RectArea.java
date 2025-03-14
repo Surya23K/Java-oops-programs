3e180392eeedeae95dc3e21bc6ac72a0592aaaea
Surya23K <suryaa232002@gmail.com>
2025-03-14 03:17:53 -0700

    Add files via upload

---
+++ RectArea.java
@@ -0,0 +1,18 @@
+public class RectArea
+{
+double l,b;
+void getData(double length,double breadth)
+{
+    l=length;
+    b=breadth;
+}
+    double calArea()
+    {
+        return l*b;
+    }
+    public static void main(String[] args) {
+        RectArea r=new RectArea();
+        r.getData(12.38,13);
+        System.out.println("Area of rectangle="+r.calArea());
+    }
+}
