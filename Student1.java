ba5c05612d7744903f3c4fcc4e71cfa532b1cef4
Surya23K <suryaa232002@gmail.com>
2025-03-14 03:17:33 -0700

    Add files via upload

---
+++ Student1.java
@@ -0,0 +1,10 @@
+class Student1 {
+    void getinput(int rollno)
+    {
+        System.out.println("Roll no:"+rollno);
+    }
+    public static void main(String[] args) {
+        Student1 ob=new Student1();
+        ob.getinput(125);
+    }
+}
