0c204dbc668924ffdfd2b690775c8a29a4b5aa94
Surya23K <suryaa232002@gmail.com>
2025-03-14 03:14:24 -0700

    Add files via upload

---
+++ ObjDemo.java
@@ -0,0 +1,11 @@
+public class ObjDemo {
+    void msg()
+    {
+        System.out.println("Classes and objects");
+    }
+    public static void main(String[] args) 
+    {
+      ObjDemo ob=new ObjDemo();
+      ob.msg();  
+    }
+}
